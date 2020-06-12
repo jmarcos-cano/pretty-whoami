c58af89f1252d53a6fc3776c461f500ea2ec28c3
@jmarcos-cano <jmarcos.cano@gmail.com>
2020-06-12 14:12:16 -0600

    @jmarcos-cano - last foo latest app

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ today = date.today()
 
 app = Flask(__name__)
 
+
+quotes_list=[]
+quote_text_file="moviequotes/bttf.txt"
 def get_quote():
     limit=10
     ran= random.randint(0,limit-1)
@@ -21,12 +24,30 @@ def get_quote():
     return quote
 
 
+def get_quote_file(text_file):
+    with open(text_file,"r") as file:
+        lines=file.readlines()
+        index=1
+        for quote in lines:
+            quote=quote.rstrip().strip('"')
+            quotes_list.append(quote)
+            index+=1
+    limit=len(quotes_list)-1
+    ran= random.randint(0,limit)
+    return quotes_list[ran]
+
 @app.route('/')
 def hello():
     host=platform.node()
     extra=os.getenv("EXTRA","")
-    time=os.getenv("TIME",today)
-    return render_template('1985_index.html', hostname=host, quote=get_quote(), extra=extra,time=time)
+    time=os.getenv("JUMP_DATE",today)
+    time=str(time)
+    if "1985" in time:
+        return render_template('1985_index.html', hostname=host, quote=get_quote_file(quote_text_file), extra=extra,time=time)
+    elif "1955" in time:
+        return render_template('1955_index.html', hostname=host, quote=get_quote_file(quote_text_file), extra=extra,time=time)
+    else:
+        return render_template('index.html', hostname=host, quote=get_quote_file(quote_text_file), extra=extra,time=time)
 
 @app.route('/health')
 def health():
